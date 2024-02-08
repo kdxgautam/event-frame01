1655cd85b0b24e2d81cc5c6c00dddf85b7ab53f7
kdxgautam <kdxgautam@gmail.com>
2024-02-08 11:20:41 +0530

    made event-frame

---
+++ src/components/EventFrame.jsx
@@ -0,0 +1,35 @@
+import EventCard from "./EventCard"
+import Footer from "./Footer"
+import Navbar from "./Navbar"
+
+
+const EventFrame = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="flex w-full justify-center items-center mt-28 pt-[70px]">
+        <div className="text-center  w-[450px] min-w-[300px] flex flex-col translate-x-14">
+          <h1 className="text-[#BBBCC0] text-5xl ">
+            Lorem ipsum dolor, sit amet
+          </h1>
+          <h6 className="text-[#BBBCC0]">Lorem ipsum dolor, sit amet</h6>
+        </div>
+
+        <div className="w-1/2 items-center flex justify-center h-3/4 -translate-x-14">
+          <img className="relative top-[-200px] right-[-120px]" src="/images/vector.png" alt="" />
+          <img className="min-w-[310px]" src="/images/robot.png" alt="" />
+          <img className="relative top-[100px] right-[]" src="/images/vector2.png" alt="" />
+        </div>
+        
+      </div>
+      <div className="flex flex-col w-full items-center my-20">
+        <EventCard />
+        <EventCard />
+        <EventCard />
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default EventFrame
